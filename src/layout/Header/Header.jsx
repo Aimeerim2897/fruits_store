b299956bdb9785f91c370622c6cb4ef8c7e74b30
Daniel <danzykgz@gmail.com>
2023-08-26 12:13:54 +0300

    update me

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,17 +1,53 @@
-import './Header.module.css'
+import cls from './Header.module.scss'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div className='header'>
-      <div className='textHed'>
-        <div className='logo'>
-          <img src="" alt="" />
+    <div className={cls.header}>
+      <div className={cls.boxHed}>
+        <div className={cls.logoHed}>
+          <div className={cls.logoHedIcone}>
+            <img src="/src/assets/Logo.png" alt="" />
+            <h1>Organick</h1>
+          </div>
+          <div className={cls.menuHedText}>
+            <p>
+              <Link to={'/'}>
+                Home
+              </Link>
+              <Link to={'/'}>
+                About
+              </Link>
+              <Link to={'/'}>
+                Pages
+              </Link>
+              <Link to={'/'}>
+                Shop
+              </Link>
+              <Link to={'/'}>
+                Projects
+              </Link>
+              <Link to={'/'}>
+                News
+              </Link>
+              </p>
+          </div>
         </div>
-        <div className='logotext'></div>
-      </div>
-      <div className='iconeHed'>
-        <div className='poiskBtn'></div>
-        <div className='korzinka'></div>
+
+        <div className={cls.activeHed}>
+          <div className={cls.searchBtn}>
+            <input type="text" />
+            <img src="/src/assets/Search Icon.png" alt="" />
+          </div>
+          <div className={cls.cartHed}>
+            <div>
+              <img src="/src/assets/Cart Icon.png" alt="" />
+              <p>
+                Cart (0)
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
