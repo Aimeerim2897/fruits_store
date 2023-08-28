b11c276c5cb39c8a5e052947d8d1b864aae2f0c0
Karimova1410 <138432927+Karimova1410@users.noreply.github.com>
2023-08-28 13:55:38 +0600

    shopSingle done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './layout/Layout';
 import ShopSingle from './pages/ShopSingle/ShopSIngle';
+import Cart from './pages/Cart/Cart';
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <Route path='/' element={<Layout />}>
           <Route index element={<h1>Home Page</h1>} />
           <Route path='/shopSingle' element={<ShopSingle/>} />
+          <Route path='/cart' element={<Cart/>} />
         </Route>
       </Routes>
     </div>
